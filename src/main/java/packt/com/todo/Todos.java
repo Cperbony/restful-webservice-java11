a067069df17cecc20b15aacb0959f3ba8d347116
Claudinei Perboni <cperbony@gmail.com>
2019-04-16 23:35:56 -0300

    Persistence Todos

--- src/main/java/packt/com/todo/Todos.java
+++ src/main/java/packt/com/todo/Todos.java
@@ -1,18 +1,56 @@
 package packt.com.todo;
 
-import java.util.HashMap;
+import org.dalesbred.query.SqlQuery;
+import packt.com.database.DB;
+
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import static org.dalesbred.query.SqlQuery.namedQuery;
+import static org.dalesbred.query.SqlQuery.query;
 
 public class Todos {
 
-    public static Map<Long, Todo> todos = new HashMap<>();
-
-    static {
-        todos.put(1L, new Todo(1L, "first Todo"));
-        todos.put(2L, new Todo(2L, "second Todo"));
+    public static List<Todo> list() {
+        return DB.db.findAll(Todo.class, "SELECT * FROM Todos");
     }
 
-    public static Long nextId() {
-        return todos.keySet().stream().reduce(Math::max).orElse(0L) + 1L;
+    public static Todo fetch(Long id) {
+        Optional<Todo> maybeTodo = getOptionalTodo(id);
+        return getOptionalTodo(id).orElse(null);
+    }
+
+    public static boolean exists(Long id) {
+        return getOptionalTodo(id).isPresent();
+    }
+
+    private static Optional<Todo> getOptionalTodo(Long id) {
+        SqlQuery query = namedQuery("SELECT * FROM Todos WHERE id = :id",
+                Map.of("id", id));
+        return DB.db.findOptional(Todo.class, query);
+    }
+
+    public static Todo update(Todo todo) {
+        if (todo.getId() == null) {
+            //create a new Todo
+            SqlQuery query = namedQuery("INSERT INTO Todos (text) VALUES (:text)", todo);
+            Long id = DB.db.updateAndProcessGeneratedKeys(result -> {
+                result.next();
+                return result.getLong(1);
+            }, List.of("id"), query);
+            todo.setId(id);
+        } else {
+            //update this Todo
+            DB.db.update(namedQuery("UPDATE Todos SET text = :text", todo));
+        }
+        return todo;
+    }
+
+    public static Todo delete(Long id) {
+        Todo todo = fetch(id);
+        DB.db.update(namedQuery("DELETE FROM Todos WHERE id = :id", Map.of("id", id)));
+        return todo;
     }
 }
+
